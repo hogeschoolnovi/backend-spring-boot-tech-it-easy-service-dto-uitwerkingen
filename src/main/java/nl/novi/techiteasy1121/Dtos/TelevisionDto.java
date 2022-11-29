ebb889bfaef45fe9ad41c179e868c0ceec6801dd
MRensen <markrensen@hotmail.com>
2022-11-29 13:48:04 +0100

    Ãadded comment

--- src/main/java/nl/novi/techiteasy1121/Dtos/TelevisionDto.java
+++ src/main/java/nl/novi/techiteasy1121/Dtos/TelevisionDto.java
@@ -1,6 +1,7 @@
 package nl.novi.techiteasy1121.Dtos;
 
 // Deze klasse wordt gebruikt in je Get, Post en Put methodes. Overal waat je een Television als returnwaarde wilt geven.
+// Een input DTO behoet geen validatie
 
 public class TelevisionDto {
     private Long id;
